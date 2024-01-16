5334e0d059de499a44b301d4df83fae55e56f2b0
Rachana uniyal <rachanauniyal@Rachanas-MacBook-Air.local>
2024-01-16 10:53:49 +0530

    initial setup done

---
+++ app/home.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, View, Text } from "react-native";
+
+
+const Home = () =>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>
+                    Grocery Store
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
